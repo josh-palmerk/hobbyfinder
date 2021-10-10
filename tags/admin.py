df7e617b192010a004e17e6eeb2cc3571a828ed3
Koltasaurusrex <koltonheaps@gmail.com>
2021-10-09 19:31:35 -0600

    EventTag and UserTag relation made

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, EventTag, UserTag
+
+admin.site.register(Tag)
+admin.site.register(EventTag)
+admin.site.register(UserTag)
--- tags/models.py
+++ tags/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from events.models import Event
+from events.models import Event
 
 # Create your models here.
 
@@ -10,13 +10,13 @@ class Tag(models.Model):
     name = models.CharField(max_length=50)
 
 
-# class EventTags(models.Model):
-#
-#     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-#     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-#
-#
-# class UserTags(models.Model):
-#
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class EventTag(models.Model):
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+class UserTag(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
